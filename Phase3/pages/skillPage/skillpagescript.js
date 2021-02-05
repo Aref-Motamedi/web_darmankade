c67cfe53eece175b07dc97984a93fab2797f1013
Aref Motamedi <aref78.m@gmail.com>
2021-02-06 02:10:53 +0330

    skill page slideshow is ok now

--- Phase3/pages/skillPage/skillpagescript.js
+++ Phase3/pages/skillPage/skillpagescript.js
@@ -1,8 +1,29 @@
+slideshowindex=0;
+slides = document.getElementsByClassName("myslides");
+for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+}
+
 function listredirect(){
     window.location="../listPage/neurologist.html";
 }
 
+function slideShowchange(){
+    
+    window.setTimeout("slideShowchange()", 5000); // Change image every 5 seconds
+    if (slideshowindex>0){
+        slides[slideshowindex-1].style.display = "none"; 
+    }
+    slideshowindex++;
+    if (slideshowindex > slides.length) {slideshowindex = 1}    
+    slides[slideshowindex-1].style.display = "block";  
+    console.log(slideshowindex);
+    
+    
+}
+
 function main(){
+    slideShowchange();
 
 }
 main();
