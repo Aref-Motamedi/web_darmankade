6623733b723041af5275f07a43b0fbeb5c7d5246
Aref Motamedi <aref78.m@gmail.com>
2021-02-08 21:17:58 +0330

    a new thing added for hosein to test

--- Phase3/pages/listPage/neurologistscript.js
+++ Phase3/pages/listPage/neurologistscript.js
@@ -22,6 +22,16 @@ async function fetchdocs(searchkey){
         // ERROR HANDLING
         window.alert(`Error while fetching doc`);
     }
+
+    respond=null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log(this.response);
+      }
+    };
+    xmlhttp.open("GET", "/web_darmankadeh/public/searchDoctors?name="+searchkey, false);
+    xmlhttp.send();
         
     // }
 
